ca1a2afa9a1651e696efe021d2eac200e2af013a
Carlos Cadena <cs.cadena@gmail.com>
2018-08-13 16:55:59 -0700

    displaying results after survey and removing images

--- js/app.js
+++ js/app.js
@@ -8,7 +8,8 @@ var displayPicThree = document.getElementById('third-pic');
 
 var allProducts = [];
 // constuctor for items
-function Product(pathTo) {
+function Product(item, pathTo) {
+  this.item = item;
   this.pathTo = pathTo;
   this.timesShown = 0;
   this.votes = 0;
@@ -16,26 +17,26 @@ function Product(pathTo) {
 }
 
 // create new products
-var bag = new Product('img/bag.jpg');
-var banana = new Product('img/banana.jpg');
-var bathroom = new Product('img/bathroom.jpg');
-var boots = new Product('img/boots.jpg');
-var breakfast = new Product('img/breakfast.jpg');
-var bubblegum = new Product('img/bubblegum.jpg');
-var chair = new Product('img/chair.jpg');
-var cthulhu = new Product('img/cthulhu.jpg');
-var dogDuck = new Product('img/dog-duck.jpg');
-var dragon = new Product('img/dragon.jpg');
-var pen = new Product('img/pen.jpg');
-var petSweep = new Product('img/pet-sweep.jpg');
-var scissors = new Product('img/scissors.jpg');
-var shark = new Product('img/shark.jpg');
-var sweep = new Product('img/sweep.png');
-var tauntaun = new Product('img/tauntaun.jpg');
-var unicorn = new Product('img/unicorn.jpg');
-var usb = new Product('img/usb.gif');
-var waterCan = new Product('img/water-can.jpg');
-var wineGlass = new Product('img/wine-glass.jpg');
+var bag = new Product('R2 Bag', 'img/bag.jpg');
+var banana = new Product('Banana Slicer','img/banana.jpg');
+var bathroom = new Product('Toilet Paper/i-pad Holder','img/bathroom.jpg');
+var boots = new Product('Open Toed Rainboots','img/boots.jpg');
+var breakfast = new Product('All-in-One Breakfast Maker','img/breakfast.jpg');
+var bubblegum = new Product('Meatball bubblegum','img/bubblegum.jpg');
+var chair = new Product('Round Bottom Chair','img/chair.jpg');
+var cthulhu = new Product('Cthulhu','img/cthulhu.jpg');
+var dogDuck = new Product('Dog duck-bill','img/dog-duck.jpg');
+var dragon = new Product('Dragon Meat','img/dragon.jpg');
+var pen = new Product('Utensil Pen','img/pen.jpg');
+var petSweep = new Product('Pet Sweeper','img/pet-sweep.jpg');
+var scissors = new Product('Pizza Scissors','img/scissors.jpg');
+var shark = new Product('Shark Sleeping Bag','img/shark.jpg');
+var sweep = new Product('Baby Sweeper','img/sweep.png');
+var tauntaun = new Product('Tauntaun','img/tauntaun.jpg');
+var unicorn = new Product('Unicorn Meat','img/unicorn.jpg');
+var usb = new Product('Wiggling Tentacle Usb','img/usb.gif');
+var waterCan = new Product('Water Can','img/water-can.jpg');
+var wineGlass = new Product('Wine Glass','img/wine-glass.jpg');
 
 var newThree = [];
 var lastThree = [];
@@ -61,6 +62,18 @@ function displayNewThree(){
   displayPicThree.addEventListener('click', handleClick);
 }
 
+function showResults(){
+  var resultsList = document.getElementById('list-results')
+
+  for(var i = 0; i < allProducts.length; i++){
+    var newLi = document.createElement('li');
+    newLi.textContent = `${allProducts[i].item} - times shown: ${allProducts[i].timesShown} - selected ${allProducts[i].votes}`;
+    resultsList.appendChild(newLi);
+  }
+  document.getElementById('pic-container').setAttribute('style', 'display:none')
+
+}
+
 var clicks = 0;
 function handleClick(e){
   e.preventDefault();
@@ -78,8 +91,8 @@ function handleClick(e){
     console.log(newThree[2]);
   }
   clicks++;
-  if(clicks < 25) displayNewThree();
-  // else showResults()
+  if(clicks < 5) displayNewThree();
+  else showResults();
 }
 
 
